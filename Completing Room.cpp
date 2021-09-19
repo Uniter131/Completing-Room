cae402fb1c0a0751a2a97fb204257c5464c1ed22
김현우 <blgre255@gmail.com>
2021-09-19 21:40:48 +0900

    주석추가

--- Completing Room.cpp
+++ Completing Room.cpp
@@ -16,6 +16,7 @@ typedef struct Puzzzle {
     ObjectPtr piece;
 } Puzzle;
 
+// 퍼즐조각의 위치와 이미지를 서로 바꿉니다
 void ChangePieces(Puzzle* pieceA, Puzzle* pieceB) {
     int tempLoc;
     string temp;
@@ -32,6 +33,7 @@ void ChangePieces(Puzzle* pieceA, Puzzle* pieceB) {
     pieceB->locate = tempLoc;
 }
 
+// 퍼즐조각의 원래 위치를 찾아 반환합니다
 int FindIndex(Puzzle* sidePiece, int loc) {
     for (int i = 0; i < MAX_INDEX - 1; i++) {
         if (sidePiece[i].locate == loc) return i;
@@ -39,6 +41,7 @@ int FindIndex(Puzzle* sidePiece, int loc) {
     return MAX_INDEX - 1;
 }
 
+// 퍼즐이 완성되었는지 검사합니다
 bool IsCompleted(Puzzle* piece) {
     for (int i = 0; i < MAX_INDEX; i++) {
         if (piece[i].locate != i) return false;
@@ -75,6 +78,7 @@ int main()
 
     Puzzle puzzle[MAX_INDEX];
 
+    // play버튼이 눌리면 playScene을 열고 관련 UI를 활성화합니다
     button_play->setOnMouseCallback([&](auto object, auto x, auto y, auto action)->bool {
         playScene->enter();
         object->hide();
@@ -83,24 +87,29 @@ int main()
         return true;
     });
 
+    // exit버튼이 눌리면 게임을 종료합니다
     button_exit->setOnMouseCallback([&](auto object, auto x, auto y, auto action)->bool {
         endGame();
         return true;
     });
 
+    // 게임 시작 시 퍼즐조각들이 처음에 자리잡을 위치를 지정합니다
     for (int i = 1; i < 5; i++) {
         xPos[i] = xPos[i - 1] + INTERVAL;
         yPos[i] = yPos[i - 1] - INTERVAL;
     }
 
+    // retry버튼이 눌리면 게임을 재시작합니다
     button_retry->setOnMouseCallback([&](auto object, auto x, auto y, auto action)->bool {
         playScene->enter();
         return true;
         });
 
+    // playScene이 열리면 타이머를 시작하고 퍼즐을 지정된 위치로 배치합니다
     playScene->setOnEnterCallback([&](auto scene)->bool {
         time = 0;
         timer->start();
+        showMessage("화살표 방향기로 퍼즐을 이동할 수 있습니다");
         for (int i = 0; i < 5; i++) {
             for (int k = 0; k < 5; k++) {
                 int index = i * 5 + k;
@@ -111,6 +120,7 @@ int main()
             curLoc = puzzle[MAX_INDEX - 1].locate;
         }
 
+        // 랜덤으로 퍼즐을 재배치합니다
         std::random_device rd;
         std::mt19937 gen(rd());
         std::uniform_int_distribution<int> dis(0, MAX_INDEX - 2);
@@ -124,6 +134,7 @@ int main()
         return true;
         });
 
+    //화살표 방향키에 따라 퍼즐을 이동시킵니다
     playScene->setOnKeyboardCallback([&](auto scene, auto keyCode, auto pressed)->bool {
         if (pressed != false) {
             switch (keyCode) {
@@ -153,12 +164,15 @@ int main()
                     break;
             }
         }
+
+        // 퍼즐을 클리어했는지 검사하고 클리했다면 afterScene을 엽니다
         if (IsCompleted(&puzzle[0])) {
             afterScene->enter();
         }
         return true;
     });
 
+    // 타이머는 1초마다 종료되고 재시작합니다. 타이머가 종료될 때마다 플레이 시간이 1초 누적됩니다
     timer->setOnTimerCallback([&](TimerPtr)->bool {
         time += 1;
         timer->set(1.f);
@@ -166,6 +180,7 @@ int main()
         return true;
     });
 
+    //afterScene이 열리면 타이머를 멈추고 클리어 시간을 보여줍니다 
     afterScene->setOnEnterCallback([&](auto scene)->bool {
         afterTitle->show();
         timer->stop();
