bd69b690d7fb63036643f34160fb97c8e2fbaedf
김현우 <blgre255@gmail.com>
2021-09-26 12:54:16 +0900

    클래스를 활용하여 코드 수정

--- Completing Room.cpp
+++ Completing Room.cpp
@@ -3,6 +3,7 @@
 #include <bangtal>
 #include <string>
 #include <random>
+#include <time.h>
 
 #define INTERVAL 144
 #define MAX_INDEX 25
@@ -10,80 +11,175 @@
 using namespace bangtal;
 using namespace std;
 
-typedef struct Puzzzle {
-    int locate;
-    string imageName;
+class Puzzle {
+private:
+    int index;
+    string imgName;
+    ScenePtr scene;
     ObjectPtr piece;
-} Puzzle;
 
-// 퍼즐조각의 위치와 이미지를 서로 바꿉니다
-void ChangePieces(Puzzle* pieceA, Puzzle* pieceB) {
-    int tempLoc;
-    string temp;
+public:
+    int getIndex() const { return index; }
+    string getImgName() const { return imgName; }
+    ObjectPtr getPiece() const { return piece; }
 
-    temp = pieceA->imageName;
-    pieceA->imageName = pieceB->imageName;
-    pieceB->imageName = temp;
+    void setIndex(const int i) { index = i; }
+    void setImageName(const string& iN) { imgName = iN; };
+    void InitPiece(ScenePtr s, int x, int y, int i);
+};
 
-    pieceA->piece->setImage(pieceA->imageName);
-    pieceB->piece->setImage(pieceB->imageName);
+class Board {
+private:
+    int xPos, yPos;
 
-    tempLoc = pieceA->locate;
-    pieceA->locate = pieceB->locate;
-    pieceB->locate = tempLoc;
+public:
+    int getX() const { return xPos; };
+    int getY() const { return yPos; };
+
+    void setPos(const int x, const int y) { xPos = x; yPos = y; };
+
+    void InitBoard(int x, int y, int i) { xPos = x; yPos = y; };
+};
+
+ScenePtr openingScene;
+ScenePtr playScene;
+ScenePtr afterScene;
+
+ObjectPtr openTitle;
+ObjectPtr button_play;
+ObjectPtr button_exit;
+ObjectPtr button_retry;
+ObjectPtr sampleImage;
+ObjectPtr afterTitle;
+ObjectPtr clear;
+
+TimerPtr timer;
+TimerPtr timerShow;
+
+Puzzle piece[MAX_INDEX];
+Board board[MAX_INDEX];
+int blank = MAX_INDEX - 1;
+
+// 호출될시 Puzzle 클래스의 멤버들을 초기화하고 퍼즐조각을 생성합니다
+void Puzzle::InitPiece(ScenePtr s, int x, int y, int i) {
+    scene = s; index = i;
+    imgName = "Images/Image1-" + to_string(i + 1) + ".png";
+    piece = Object::create(imgName, s, x, y);
 }
 
-// 퍼즐조각의 원래 위치를 찾아 반환합니다
-int FindIndex(Puzzle* sidePiece, int loc) {
-    for (int i = 0; i < MAX_INDEX - 1; i++) {
-        if (sidePiece[i].locate == loc) return i;
-    }
-    return MAX_INDEX - 1;
-}
-
-// 퍼즐이 완성되었는지 검사합니다
-bool IsCompleted(Puzzle* piece) {
-    for (int i = 0; i < MAX_INDEX; i++) {
-        if (piece[i].locate != i) return false;
-    }
-    return true;
-}
-
-int main()
-{
+// 씬, 오브젝트, 타이머를 생성합니다
+void InitGame() {
     setGameOption(GameOption::GAME_OPTION_ROOM_TITLE, false);
     setGameOption(GameOption::GAME_OPTION_INVENTORY_BUTTON, false);
     setGameOption(GameOption::GAME_OPTION_MESSAGE_BOX_BUTTON, false);
 
-    auto openingScene = Scene::create("", "Images/StartBackground.png");
-    auto playScene = Scene::create("", "Images/PlayBackground.png");
-    auto afterScene = Scene::create("", "Images/StartBackground.png");
+    openingScene = Scene::create("", "Images/StartBackground.png");
+    playScene = Scene::create("", "Images/PlayBackground.png");
+    afterScene = Scene::create("", "Images/StartBackground.png");
 
-    auto openTitle = Object::create("Images/Title.png", openingScene, 40, 450);
-    auto button_play = Object::create("Images/Play.png", openingScene, 280, 320);
-    auto button_exit = Object::create("Images/Exit.png", openingScene, 280, 250);
+    openTitle = Object::create("Images/Title.png", openingScene, 40, 450);
+    button_play = Object::create("Images/Play.png", openingScene, 280, 320);
+    button_exit = Object::create("Images/Exit.png", openingScene, 280, 250);
+    button_retry = Object::create("Images/Retry.png", playScene, 780, 80);
+    sampleImage = Object::create("Images/Image1.png", playScene, 775, 450);
+    afterTitle = Object::create("Images/Title.png", afterScene, 40, 500);
+    clear = Object::create("Images/Clear.png", afterScene, 200, 400);
 
-    auto button_retry = Object::create("Images/Retry.png", playScene, 780, 80);
-    auto sampleImage = Object::create("Images/Image1.png", playScene, 775, 450);
+    timer = Timer::create(1.f);
+    timerShow = Timer::create(0.f);
+}
 
-    auto afterTitle = Object::create("Images/Title.png", afterScene, 40, 500);
-    auto clear = Object::create("Images/Clear.png", afterScene, 200, 400);
+// Board 클래스의 멤버들을 초기화하고 playScene에 퍼즐조각을 생성합니다
+void CreatePieces() {
+    for (int i = 0; i < MAX_INDEX; i++) {
+        int x = (i % 5) * INTERVAL;
+        int y = 576 - (i / 5) * INTERVAL;
+        board[i].InitBoard(x, y, i);
+        piece[i].InitPiece(playScene, x, y, i);
+    }
+}
 
-    auto timer = Timer::create(1.f);
+// 몇번째 퍼즐조각인지 찾고 그 값을 반환합니다
+int FindIndex(int index) {
+    for (int i = 0; i < MAX_INDEX; i++) {
+        if (piece[i].getIndex() == index) return i;  
+    }
+    return -1; //오류
+}
 
-    int xPos[5] = { 0, };
-    int yPos[5] = { 576, };
-    int curLoc;
+// 퍼즐조각을 서로 바꿉니다
+void ChangePieces(int numA, int numB) {
+    int a = MAX_INDEX - 1;
+    int b = FindIndex(numB);
+    int tempIndex = piece[a].getIndex();
+
+    piece[a].getPiece()->locate(playScene, board[numB].getX(), board[numB].getY());  // 바꾸는 조각의 위치로 locate
+    piece[b].getPiece()->locate(playScene, board[numA].getX(), board[numA].getY());  // 바꾸는 조각의 위치로 locate
+
+    piece[a].setIndex(piece[b].getIndex());
+    piece[b].setIndex(tempIndex);
+}   
+
+// 빈칸이 지정된 경로로 이동하며, 만난 퍼즐조각들을 바꿉니다
+void MoveToRoute(int st, int nd) {
+    int xInterval = (st % 5 - nd % 5 > 0 ? st % 5 - nd % 5 : nd % 5 - st % 5);
+    int yInterval = (st / 5 - nd / 5 > 0 ? st / 5 - nd / 5 : nd / 5 - st / 5);
+    int start = st;
+    int end = nd;
+
+    for (int i = 0; i < xInterval; i++) {
+        int direction = (start % 5 - nd % 5 > 0 ? -1 : 1);  // 빈칸이 움직일 방향 설정
+        end = start + direction;
+        ChangePieces(start, end);
+        start = end;
+    }
+
+    for (int i = 0; i < yInterval; i++) {
+        int direction = (st / 5 - nd / 5 > 0 ? -1 : 1);  // 빈칸이 움직일 방향 설정
+        end = start + (direction * 5);
+        ChangePieces(start, end);
+        start = end;
+    }
+}
+
+/* 랜덤으로 퍼즐을 재배치합니다. 빈칸이 랜덤한 경로를 여러번 이동하며 마지막에는 오른쪽 아래 구석으로 이동합니다. 이 과정에서 퍼즐이 랜덤하게 섞이게 됩니다
+*  이 방식으로 퍼즐을 섞었더니 타이머로 애니메이션을 넣기 어려워졌습니다... 빈칸은 목적지까지 X축으로 먼저 움직였다가 그 후에 Y축으로 움직입니다.
+*  이렇게 실행 분기가 나뉘어 타이머 두 개를 활용해 애니메이션을 구현하려 했으나 방식의 문제인지 잘 되지 않았습니다. 어떤 방식으로 애니메이션을 구현할 수 있을지 궁금합니다.
+*/
+void RandomSetting() {
+    int end;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dis(0, MAX_INDEX - 2);
+
+    for (int i = 0; i < 15; i++) {
+        end = dis(gen);  
+        MoveToRoute(blank, end);   // 빈칸 위치에서 랜덤한 위치로 이동
+        blank = end;
+    }
+    MoveToRoute(blank, MAX_INDEX - 1);  // 이동이 끝나면 마지막으로 오른쪽 아래 구석으로 이동
+    blank = MAX_INDEX - 1;
+}
+// 퍼즐이 완성되었는지 검사합니다
+bool IsCompleted() {
+    for (int i = 0; i < MAX_INDEX; i++) {
+        if (piece[i].getIndex() != i) return false;
+    }
+    return true;
+}
+
+int main()                                                                                                                                                                                                                                                                            {
     int time = 0;
-
-    Puzzle puzzle[MAX_INDEX];
+    InitGame();
 
     // play버튼이 눌리면 playScene을 열고 관련 UI를 활성화합니다
-    button_play->setOnMouseCallback([&](auto object, auto x, auto y, auto action)->bool {
-        playScene->enter();
-        object->hide();
+    button_play->setOnMouseCallback([&](auto playButton, auto x, auto y, auto action)->bool {
+        playButton->hide();
         button_exit->hide();
-        clear->hide();
+        clear->hide();   
+        playScene->enter();
+        showMessage("화살표 방향키로 퍼즐을 이동할 수 있습니다");
         return true;
     });
 
@@ -93,88 +189,63 @@ int main()
         return true;
     });
 
-    // 게임 시작 시 퍼즐조각들이 처음에 자리잡을 위치를 지정합니다
-    for (int i = 1; i < 5; i++) {
-        xPos[i] = xPos[i - 1] + INTERVAL;
-        yPos[i] = yPos[i - 1] - INTERVAL;
-    }
-
     // retry버튼이 눌리면 게임을 재시작합니다
     button_retry->setOnMouseCallback([&](auto object, auto x, auto y, auto action)->bool {
         playScene->enter();
         return true;
-        });
+    });
 
-    // playScene이 열리면 타이머를 시작하고 퍼즐을 지정된 위치로 배치합니다
+    // playScene이 열리면 타이머를 시작하고 퍼즐을 랜덤한 위치로 배치합니다
     playScene->setOnEnterCallback([&](auto scene)->bool {
+        CreatePieces();
         time = 0;
         timer->start();
-        showMessage("화살표 방향기로 퍼즐을 이동할 수 있습니다");
-        for (int i = 0; i < 5; i++) {
-            for (int k = 0; k < 5; k++) {
-                int index = i * 5 + k;
-                puzzle[index].imageName = "Images/Image1-" + to_string(index + 1) + ".png";
-                puzzle[index].piece = Object::create(puzzle[index].imageName, playScene, xPos[k], yPos[i]);
-                puzzle[index].locate = index;
-            }
-            curLoc = puzzle[MAX_INDEX - 1].locate;
-        }
-
-        // 랜덤으로 퍼즐을 재배치합니다
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<int> dis(0, MAX_INDEX - 2);
-
-        for (int i = 0; i < MAX_INDEX - 1; i++) {
-            int num = dis(gen);
-            int idx1 = FindIndex(&puzzle[0], i);
-            int idx2 = FindIndex(&puzzle[0], num);
-            ChangePieces(&puzzle[idx1], &puzzle[idx2]);
-        }
+        showTimer(timerShow);
+        RandomSetting();    // 이 부분을 주석처리하면 퍼즐을 쉽게 클리어할 수 있습니다.
         return true;
-        });
-
-    //화살표 방향키에 따라 퍼즐을 이동시킵니다
+    });
+    //화살표 방향키에 따라 빈칸을 이동시킵니다
     playScene->setOnKeyboardCallback([&](auto scene, auto keyCode, auto pressed)->bool {
         if (pressed != false) {
             switch (keyCode) {
                 case KeyCode::KEY_LEFT_ARROW:
-                    if (curLoc % 5 != 4) {
-                        ChangePieces(&puzzle[curLoc], &puzzle[curLoc + 1]);
-                        curLoc += 1;
+                    if (blank % 5 != 0) {
+                        ChangePieces(blank, (blank - 1));
+                        blank -= 1;
                     }
                     break;
                 case KeyCode::KEY_RIGHT_ARROW:
-                    if (curLoc % 5 != 0) {
-                        ChangePieces(&puzzle[curLoc], &puzzle[curLoc - 1]);
-                        curLoc -= 1;
+                    if (blank % 5 != 4) {
+                        ChangePieces(blank, (blank + 1));
+                        blank += 1;
                     }
                     break;
                 case KeyCode::KEY_UP_ARROW:
-                    if (curLoc / 5 != 4) {
-                        ChangePieces(&puzzle[curLoc], &puzzle[curLoc + 5]);
-                        curLoc += 5;
+                    if (blank / 5 != 0) {
+                        ChangePieces(blank, (blank - 5));
+                        blank -= 5;
                     }
                     break;
                 case KeyCode::KEY_DOWN_ARROW: 
-                    if (curLoc / 5 != 0) {                   
-                        ChangePieces(&puzzle[curLoc], &puzzle[curLoc - 5]);
-                        curLoc -= 5;
+                    if (blank / 5 != 4) {
+                        ChangePieces(blank, (blank + 5));
+                        blank += 5;
                     }
                     break;
             }
         }
 
-        // 퍼즐을 클리어했는지 검사하고 클리했다면 afterScene을 엽니다
-        if (IsCompleted(&puzzle[0])) {
+        // 퍼즐을 완성했는지 검사하고 완성했다면 afterScene을 엽니다
+        if (IsCompleted()) {
             afterScene->enter();
         }
         return true;
     });
 
-    // 타이머는 1초마다 종료되고 재시작합니다. 타이머가 종료될 때마다 플레이 시간이 1초 누적됩니다
+    // 타이머는 1초마다 종료되고 재시작합니다. 타이머가 종료될 때마다 플레이 시간이 1초 누적됩니다. 누적된 시간은 다른 타이머를 통해 게임화면에 나타납니다
     timer->setOnTimerCallback([&](TimerPtr)->bool {
         time += 1;
+        timerShow->increase(1.f);
         timer->set(1.f);
         timer->start();
         return true;
